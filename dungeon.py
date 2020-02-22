83447dfb05a7b5b1be98d5ff5aa8721b911850ac
redps <psafaei@nevada.unr.edu>
2020-02-22 02:23:16 -0800

    intial commit, much work to be done

---
+++ dungeon.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import filedialog, Text 
+import os 
+
+# the body, the whole structure 
+root = tk.Tk()
+
+apps = []
+
+def addApp():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                      filetypes=(("executetables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+
+
+'''
+# for linux or MacOS
+def runApps():
+    for app in apps:
+        if sys.platform == "win32":
+            os.startfile(app)
+        else:
+            opener="open" if sys.platform == "darwin" else "xdg-open"
+            subprocess.call([opener,app])
+'''
+# create a canvas to get a better environment in the root 
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+# attach the canvas 
+canvas.pack()
+
+# attach another container to the canvas 
+frame = tk.Frame(root, bg="white")
+# place it and center it 
+frame.place(relwidth=.8, relheight=.8, relx=.1, rely=.1)
+
+# Button for opening files 
+openFile = tk.Button(root, text="Open File", padx=10, 
+                     pady=5, fg="white", bg="red", command=addApp)
+openFile.pack()
+
+# Button for running apps 
+RunApps = tk.Button(root, text="Run Apps", padx=10, 
+                    pady=5, fg="white", bg="red")
+RunApps.pack()
+
+root.mainloop()
